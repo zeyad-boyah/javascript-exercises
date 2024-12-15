6599221d1044095b4ca897b08c710b98cdd2aec9
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2024-12-15 22:51:15 +0200

    added the solution for 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num_of_repeats) {
+    if (num_of_repeats <0){
+        return "ERROR"
+    }
+    let repeated_word ="";
+    for (i=0; i<num_of_repeats; i++){
+        repeated_word += word;
+    }
+    return repeated_word
 };
 
 // Do not edit below this line
